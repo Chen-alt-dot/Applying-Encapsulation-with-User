91a38d3f7a34c1294d230aac833f1a3de6bf2821
ACT-STUDENT <ACT-STUDENT@LAB3-PC39>
2025-08-27 10:37:14 +0800

    Code

---
+++ Dumdum_EncapsulationUserInput/Dumdum_EncapsulationUserInput/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dumdum_EncapsulationUserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Begin:
+            Console.Clear();
+
+            // Userinput for brand and size
+            Console.WriteLine("Your Shoe Info!!!");
+            Console.Write("Input Brand: ");
+            string brand = Console.ReadLine();
+            Console.Write("Input Size: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            // Instantiate an object
+            Shoe shoe = new Shoe { Brand = brand, Size = size };
+            // Method
+            shoe.DisplayShoeInfo();
+
+            //User choice if it want to continue or not
+            Console.WriteLine("Do you want to try another shoe? (Yes or No)");
+            string userinfo = Console.ReadLine().ToLower();
+
+            if (userinfo == "yes")
+            {
+                goto Begin;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ Dumdum_EncapsulationUserInput/Dumdum_EncapsulationUserInput/Shoe.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dumdum_EncapsulationUserInput
+{
+    internal class Shoe
+    {
+        // Name: Christian Benedict P. Dumdum
+        // Section: IT301
+        // EXER_05: Applying Encapsulation with User
+
+        //Private Fields
+        private string brand;
+        private int size;
+
+
+        // Public property for Brand
+        public string Brand { get; set; }
+        // Public property for Size
+        public int Size { get; set; }
+
+        //Method
+        public void DisplayShoeInfo()
+        {
+            Console.WriteLine("Your Brand is " + Brand + " the size is " + Size);
+        }
+    }
+}
